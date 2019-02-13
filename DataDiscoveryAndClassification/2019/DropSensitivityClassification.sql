93c8b1e163f53acc9315f74fc4512d0fdc96e96a
Darko-Martinovic <darko.martinovic@outlook.com>
2019-02-13 12:42:36 +0100

    Added SP DropSensitivityClassification

--- DataDiscoveryAndClassification/2019/DropSensitivityClassification.sql
+++ DataDiscoveryAndClassification/2019/DropSensitivityClassification.sql
@@ -1,4 +1,4 @@
-﻿CREATE PROCEDURE [DC].[AddSensitivityClassification]
+﻿CREATE PROCEDURE [DC].[DropSensitivityClassification]
 AS
 BEGIN
 SET NOCOUNT ON;
@@ -27,11 +27,8 @@ DECLARE @maxId AS INT;
 DECLARE @cSchema AS SYSNAME;
 DECLARE @cTable AS SYSNAME;
 DECLARE @cColumn AS SYSNAME;
-DECLARE @cInformationTypeName AS VARCHAR(50);
-DECLARE @cInformationTypeId AS VARCHAR(50);
-DECLARE @cSensitivityLabel AS VARCHAR(50);
-DECLARE @cSensitivitLableId AS VARCHAR(50);
 DECLARE @sqlString AS NVARCHAR(MAX)
+
 SET @id = 1;
 
 IF OBJECT_ID('tempdb..#TempResult') IS NOT NULL
@@ -39,16 +36,10 @@ IF OBJECT_ID('tempdb..#TempResult') IS NOT NULL
 SELECT GCC.*,
     ROW_NUMBER() OVER(ORDER BY SchemaName,
                                 TABLENAME,
-                                COLUMNNAME) RN,
-    CAST(IT.InfoTypeId AS VARCHAR(50)) InformationTypeId,
-	CAST(SN.LabelId AS VARCHAR(50)) SensitivityLabelId
+                                COLUMNNAME) RN
 INTO #TempResult
 FROM   [DC].[GetClassifiedColumns] GCC
-INNER JOIN DC.SensitiveName SN 
-	ON GCC.SensitivityLabel = SN.LabelName
-INNER JOIN DC.InformationType IT 
-	ON GCC.InformationType = IT.InfoTypeName
-WHERE GCC.IsComputed = 0
+
 
 
 	         SET @maxid =
@@ -76,44 +67,10 @@ WHERE GCC.IsComputed = 0
     FROM         #TempResult
     WHERE        rn = @id
 );
-                 SET @cInformationTypeName =
-(
-    SELECT TOP 1 InformationType
-    FROM         #TempResult
-    WHERE        rn = @id
-);
-                 SET @cInformationTypeId =
-(
-    SELECT TOP 1 InformationTypeId
-    FROM         #TempResult
-    WHERE        rn = @id
-);
-                 SET @cSensitivityLabel =
-(
-    SELECT TOP 1 SensitivityLabel
-    FROM         #TempResult
-    WHERE        rn = @id
-);
-                 SET @cSensitivitLableId =
-(
-    SELECT TOP 1 SensitivityLabelId
-    FROM         #TempResult
-    WHERE        rn = @id
-);
-
 
                 SET @ID = @ID + 1;
-				SET @sqlString = 'ADD SENSITIVITY CLASSIFICATION TO '
+				SET @sqlString = 'DROP SENSITIVITY CLASSIFICATION FROM '
 				SET @sqlString =  @sqlstring + @cSchema + '.' + @cTable + '.' + @cColumn
-				SET @sqlString =  @sqlString + ' WITH ( LABEL_ID='''
-				SET @sqlString =  @sqlString + CAST(@cSensitivitLableId AS VARCHAR(50)) + ''''
-				SET @sqlString =  @sqlString + '  ,LABEL='''
-				SET @sqlString =  @sqlString + @cSensitivityLabel + ''''
-				SET @sqlString =  @sqlString + '  ,INFORMATION_TYPE_ID='''
-				SET @sqlString =  @sqlString + @cInformationTypeId + ''''
-				SET @sqlString =  @sqlString + '  ,INFORMATION_TYPE='''
-				SET @sqlString =  @sqlString + @cInformationTypeName + ''''
-				SET @sqlString =  @sqlString + ')'
 				EXECUTE sp_executesql @sqlString;
              END;
 
