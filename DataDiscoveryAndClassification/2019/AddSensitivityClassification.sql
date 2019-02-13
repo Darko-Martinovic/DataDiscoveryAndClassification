902f46e7875a4cef9ef6f1e042c8cddbc1d1800d
Darko-Martinovic <darko.martinovic@outlook.com>
2019-02-13 11:46:27 +0100

    Added AddSensitiveClassification stored procedure

--- DataDiscoveryAndClassification/2019/AddSensitivityClassification.sql
+++ DataDiscoveryAndClassification/2019/AddSensitivityClassification.sql
@@ -1,5 +1,7 @@
 ﻿CREATE PROCEDURE [DC].[AddSensitivityClassification]
 AS
+BEGIN
+SET NOCOUNT ON;
 DECLARE @version AS VARCHAR(128)
 SET @version = 
   CASE 
@@ -11,7 +13,7 @@ SET @version =
      WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '12%' THEN 'SQL2014'
      WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '13%' THEN 'SQL2016'     
      WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '14%' THEN 'SQL2017' 
-     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '14%' THEN 'SQL2019' 
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '15%' THEN 'SQL2019' 
      ELSE 'unknown'
   END 
 IF ( @version != 'SQL2019')
@@ -19,5 +21,104 @@ BEGIN
 PRINT 'Only for SQL2019';
 RETURN 0
 END
+
+DECLARE @id AS INT;
+DECLARE @maxId AS INT;
+DECLARE @cSchema AS SYSNAME;
+DECLARE @cTable AS SYSNAME;
+DECLARE @cColumn AS SYSNAME;
+DECLARE @cInformationTypeName AS VARCHAR(50);
+DECLARE @cInformationTypeId AS VARCHAR(50);
+DECLARE @cSensitivityLabel AS VARCHAR(50);
+DECLARE @cSensitivitLableId AS VARCHAR(50);
+DECLARE @sqlString AS NVARCHAR(MAX)
+
+SET @id = 1;
+
+IF OBJECT_ID('tempdb..#TempResult') IS NOT NULL
+    DROP TABLE #TempResult;
+SELECT GCC.*,
+    ROW_NUMBER() OVER(ORDER BY SCHEMANAME,
+                                TABLENAME,
+                                COLUMNNAME) RN,
+    IT.InfoTypeId,
+	SN.LabelId
+INTO #TempResult
+FROM   [DC].[GetClassifiedColumns] GCC
+INNER JOIN DC.SensitiveName SN 
+	ON GCC.SensitivityLabel = SN.LabelName
+INNER JOIN DC.InformationType IT 
+	ON GCC.InformationType = IT.InfoTypeName
+
+
+
+	         SET @maxid =
+(
+    SELECT MAX(RN)
+    FROM   #TempResult
+);
+         WHILE @maxId >= @Id
+             BEGIN
+                 SET @cschema =
+(
+    SELECT TOP 1 SCHEMAname
+    FROM         #TempResult
+    WHERE        rn = @id
+);
+                 SET @cTable =
+(
+    SELECT TOP 1 TableName
+    FROM         #TempResult
+    WHERE        rn = @id
+);
+                 SET @cColumn =
+(
+    SELECT TOP 1 ColumnName
+    FROM         #TempResult
+    WHERE        rn = @id
+);
+                 SET @cInformationTypeName =
+(
+    SELECT TOP 1 InformationTypeName
+    FROM         #TempResult
+    WHERE        rn = @id
+);
+                 SET @cInformationTypeId =
+(
+    SELECT TOP 1 InformationTypeId
+    FROM         #TempResult
+    WHERE        rn = @id
+);
+                 SET @cSensitivityLabel =
+(
+    SELECT TOP 1 SensitivityLabelName
+    FROM         #TempResult
+    WHERE        rn = @id
+);
+                 SET @cSensitivitLableId =
+(
+    SELECT TOP 1 SensitivityLabelId
+    FROM         #TempResult
+    WHERE        rn = @id
+);
+
+
+                 SET @ID = @ID + 1;
+	SET @sqlString = 'ADD SENSITIVITY CLASSIFICATION TO '
+	SET @sqlString =  @sqlstring + @cSchema + '.' + @cTable + '.' + @cColumn
+	SET @sqlString =  @sqlString + 'WITH ( LABEL_ID='''
+	SET @sqlString =  @sqlString + CAST(@cSensitivitLableId AS VARCHAR(50)) + ''''
+    SET @sqlString =  @sqlString + '  LABEL='''
+	SET @sqlString =  @sqlString + @cSensitivityLabel + ''''
+    SET @sqlString =  @sqlString + '  INFORMATION_TYPE_ID='''
+	SET @sqlString =  @sqlString + @cInformationTypeId + ''''
+    SET @sqlString =  @sqlString + '  INFORMATION_TYPE='''
+	SET @sqlString =  @sqlString + @cInformationTypeName + ''''
+	SET @sqlString =  @sqlString + ')'
+	EXECUTE sp_executesql @sqlString;
+             END;
+
+     END; 
+
 
 RETURN 0
