00c348ad99150f8cef14fc7461c55cbc60b72a83
Darko-Martinovic <darko.martinovic@outlook.com>
2019-02-13 11:28:44 +0100

    Added new stored procedure

---
+++ DataDiscoveryAndClassification/2019/AddSensitivityClassification.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [DC].[AddSensitivityClassification]
+AS
+DECLARE @version AS VARCHAR(128)
+SET @version = 
+  CASE 
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '8%' THEN 'SQL2000'
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '9%' THEN 'SQL2005'
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '10.0%' THEN 'SQL2008'
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '10.5%' THEN 'SQL2008 R2'
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '11%' THEN 'SQL2012'
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '12%' THEN 'SQL2014'
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '13%' THEN 'SQL2016'     
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '14%' THEN 'SQL2017' 
+     WHEN CONVERT(VARCHAR(128), SERVERPROPERTY ('productversion')) like '14%' THEN 'SQL2019' 
+     ELSE 'unknown'
+  END 
+IF ( @version != 'SQL2019')
+BEGIN
+PRINT 'Only for SQL2019';
+RETURN 0
+END
+
+RETURN 0
